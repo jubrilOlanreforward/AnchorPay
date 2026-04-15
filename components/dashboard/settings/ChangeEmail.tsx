9e3b1aad5a0a016e3a249886997362c14692e9d8
LawalH-DCoder <hamzat.lawal@interswitchng.com>
2026-04-15 11:46:01 +0100

    fix: Fixed profile change email and change phone

--- components/dashboard/settings/ChangeEmail.tsx
+++ components/dashboard/settings/ChangeEmail.tsx
@@ -35,7 +35,7 @@ const ChangeEmail = () => {
   });
 
   const onSubmit = async (data: changeEmailFormValues) => {
-    router.push(`/settings/enter-email-otp?param=${data.oldEmail}`);
+    router.push(`/settings/profile/enter-email-otp?param=${data.oldEmail}`);
   };
 
   return (
